82b1aa0316fe287ec5e55f6ecbc6ed6b21730d3e
tirefire <84106878+tire-fire@users.noreply.github.com>
2023-12-15 14:51:22 -0500

    Finished day 12

---
+++ 2023/12/solve1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import re
+import numpy as np
+from collections import deque
+from itertools import combinations
+from tqdm import tqdm
+
+# with open('example', 'r') as f:
+with open("input", "r") as f:
+    inp = [x.strip() for x in f.readlines() if x.strip()]
+
+def count_arrangements(spring_row, group_sizes, memo):
+    if (spring_row, group_sizes) in memo:
+        return memo[(spring_row, group_sizes)]
+
+    def is_valid(combination):
+        count, groups = 0, []
+        for char in combination:
+            if char == "#":
+                count += 1
+            elif count > 0:
+                groups.append(count)
+                count = 0
+        if count > 0:
+            groups.append(count)
+        return tuple(groups) == group_sizes
+
+    def generate_combinations(index, spring_row):
+        if index >= len(spring_row):
+            if is_valid(spring_row):
+                return 1
+            return 0
+
+        count = 0
+        new_row = list(spring_row)
+        if new_row[index] == "?":
+            for replacement in [".", "#"]:
+                new_row[index] = replacement
+                count += generate_combinations(index + 1, ''.join(new_row))
+            new_row[index] = "?"
+        else:
+            count += generate_combinations(index + 1, spring_row)
+
+        return count
+
+    total = generate_combinations(0, spring_row)
+    memo[(spring_row, group_sizes)] = total
+    return total
+
+memo = {}
+total = 0
+for row in inp:
+    spring_row, group_sizes = row.split(" ")
+    group_sizes = tuple(int(x) for x in group_sizes.split(","))
+    total += count_arrangements(spring_row, group_sizes, memo)
+
+print(total)
---
+++ 2023/12/solve2.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import re
+import numpy as np
+from collections import deque
+from itertools import combinations
+from tqdm import tqdm
+
+# with open('example', 'r') as f:
+with open("input", "r") as f:
+    inp = [x.strip() for x in f.readlines() if x.strip()]
+
+
+def count_arrangements(spring_row, group_sizes, memo):
+    if (spring_row, group_sizes) in memo:
+        return memo[(spring_row, group_sizes)]
+
+    # base case
+    if len(group_sizes) == 0:
+        return int("#" not in spring_row)
+
+    total = 0
+    # only look at the front group by removing the groups that have to be unique
+    for position in range(
+        len(spring_row)
+        - sum(group_sizes[1:])
+        + len(group_sizes[1:])
+        - group_sizes[0]
+        + 1
+    ):
+        # the first length can be operational springs, but still have to contain a group_size of broken springs followed by an operational spring separator
+        possible = f'{"." * position}{"#" * group_sizes[0]}.'
+
+        # check each point in the spring_row with the possible and see if it's valid
+        if all(spring == possible_spring or spring == "?" for spring, possible_spring in zip(spring_row, possible)):
+            total += count_arrangements(
+                spring_row[position + group_sizes[0] + 1 :], group_sizes[1:], memo
+            )
+
+    memo[(spring_row, group_sizes)] = total
+    return total
+
+
+memo = {}
+total = 0
+for row in inp:
+    spring_row, group_sizes = row.split(" ")
+    group_sizes = tuple(int(x) for x in group_sizes.split(","))
+    unfold_row = "?".join([spring_row] * 5)
+    unfold_group = group_sizes * 5
+    total += count_arrangements(unfold_row, unfold_group, memo)
+
+print(total)
