a60d6f8dc3e3f7a51580ca02688f98fa6aacb683
tirefire <84106878+tire-fire@users.noreply.github.com>
2024-12-09 02:05:14 -0600

    finished day 9

--- 2024/9/solve1.go
+++ 2024/9/solve1.go
@@ -7,9 +7,70 @@ import (
 
 func main() {
 	lines, err := lib.ReadInput()
+	//lines, err := lib.ReadLines("exampleinput")
 	if err != nil {
 		panic("Failed to read input")
 	}
 
-	fmt.Println(lines)
+	diskMap := lines[0]
+	blocks := parseDiskMap(diskMap)
+	compacted := compactDiskMap(blocks)
+	checksum := calculateChecksum(compacted)
+	fmt.Println(checksum)
+}
+
+func parseDiskMap(diskMap string) []rune {
+	var blocks []rune
+	isFile := true
+	fileID := 0
+
+	for i := 0; i < len(diskMap); i++ {
+		length := int(diskMap[i] - '0')
+		if isFile {
+			// Add fileIDs to blocks
+			for j := 0; j < length; j++ {
+				blocks = append(blocks, rune(fileID) + '0')
+			}
+			fileID++
+		} else {
+			// Add free space blocks
+			for j := 0; j < length; j++ {
+				blocks = append(blocks, '.')
+			}
+		}
+		isFile = !isFile
+	}
+
+	return blocks
+}
+
+func compactDiskMap(blocks []rune) []rune {
+	for i := len(blocks) - 1; i >= 0; i-- {
+		// Only move if it's a file block
+		if blocks[i] != '.' {
+			// Find the leftmost free space
+			for j := 0; j < i; j++ {
+				if blocks[j] == '.' {
+					// Move the block
+					blocks[j] = blocks[i]
+					blocks[i] = '.'
+					break
+				}
+			}
+		}
+	}
+	return blocks
+}
+
+func calculateChecksum(blocks []rune) int {
+	checksum := 0
+
+	for i, block := range blocks {
+		if block != '.' {
+			fileID := int(block - '0')
+			checksum += i * fileID
+		}
+	}
+
+	return checksum
 }
--- 2024/9/solve2.go
+++ 2024/9/solve2.go
@@ -5,11 +5,95 @@ import (
 	"github.com/tire-fire/adventofcode/2024/lib"
 )
 
+type Segment struct {
+	ID     int // -1 for free space
+	Length int
+}
+
 func main() {
 	lines, err := lib.ReadInput()
+	//lines, err := lib.ReadLines("exampleinput")
 	if err != nil {
 		panic("Failed to read input")
 	}
 
-	fmt.Println(lines)
+	diskMap := lines[0]
+	blocks := parseDiskMap(diskMap)
+	compacted := compactDiskMap(blocks)
+	checksum := calculateChecksum(compacted)
+	fmt.Println(checksum)
 }
+
+
+func parseDiskMap(diskMap string) []Segment {
+	segments := []Segment{}
+	isFile := true
+	fileID := 0
+
+	for i := 0; i < len(diskMap); i++ {
+		length := int(diskMap[i] - '0')
+		if isFile {
+			segments = append(segments, Segment{ID: fileID, Length: length})
+			fileID++
+		} else {
+			segments = append(segments, Segment{ID: -1, Length: length})
+		}
+		isFile = !isFile
+	}
+
+	return segments
+}
+
+
+func compactDiskMap(segments []Segment) []Segment {
+	for i := len(segments) - 1; i >= 0; i-- {
+		segment := segments[i]
+		if segment.ID == -1 {
+			continue // Skip free space
+		}
+
+		// Find the leftmost span of free space
+		for j := 0; j < i; j++ {
+			if segments[j].ID == -1 && segments[j].Length >= segment.Length {
+				// Move the file
+				segments[j].Length -= segment.Length
+				if segments[j].Length == 0 {
+					segments = append(segments[:j], segments[j+1:]...) // Remove empty segment
+					i-- // this bug caught me for so long
+				}
+				segments[i].ID = -1
+				segments = append(segments[:j], append([]Segment{segment}, segments[j:]...)...)
+				break
+			}
+		}
+	}
+	return segments
+}
+
+func calculateChecksum(segments []Segment) int64 {
+	checksum := int64(0)
+	position := 0
+	freeSpace := false
+
+	for _, segment := range segments {
+		if freeSpace && segment.ID == -1 {
+			position += segment.Length
+			continue
+		} else {
+			freeSpace = false
+		}
+
+		if segment.ID != -1 {
+			for i := 0; i < segment.Length; i++ {
+				checksum += int64(position * segment.ID)
+				position++
+			}
+		} else {
+			position += segment.Length
+			freeSpace = true
+		}
+	}
+
+	return checksum
+}
+
